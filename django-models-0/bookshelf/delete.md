4ffa6ccfdad923a0ae1320ee749e6beab11a3735
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-10 03:25:33 +0100

    Checker

--- django-models-0/bookshelf/delete.md
+++
@@ -1,7 +0,0 @@
-# Delete Operation
-from bookshelf.models import Book
-Command:
-```python
-retrieved_book.delete()
-all_books = Book.objects.all()
-print(all_books)  # Should return an empty queryset
---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="Book")
+
+class Library(models.Model):
+    name = models.CharField(max_length=30)
+    books = models.ManyToManyField(Book, related_name="Library")  
+
+class Librarian(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="Librarian") 
