44db8c018f837e7efad42d748962cf782276224a
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-12 14:48:57 +0100

    Created Advance Features

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,6 @@ from .models import Book, CustomUser
 
 # Register your models here.
 admin.site.register(Book)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.ModelAdmin = ["list_filter", "author", "publication_year"]
 ["search_fields", "title"]
