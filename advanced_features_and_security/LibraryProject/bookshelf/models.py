df53049841946585074df46daf79d70ca8c109f1
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-14 08:25:12 +0100

    Checker

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,8 +8,15 @@ class Book(models.Model):
     author = models.CharField (max_length=100)
     publication_year = models.IntegerField()
 
-def __str__(self):
-    return f"{self.title} {self.author} {self.publication_year}"
+    def __str__(self):
+        return f"{self.title} {self.author} {self.publication_year}"
+
+    class Meta:
+        permissions = (
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        )
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True, verbose_name="Date of Birth")
