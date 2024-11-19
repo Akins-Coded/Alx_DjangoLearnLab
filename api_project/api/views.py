9cf1d4b6a329015f228263e4af4ecfbb6793ac26
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-19 13:02:31 +0100

    checker

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
-class BookList(request):
+class BookList(ListAPIView):
     BookList = Book.objects.all()
     serializer_class = BookSerializer
     
