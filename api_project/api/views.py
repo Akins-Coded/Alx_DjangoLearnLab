e6abcd0678c391caa35a313333b1bef9eb1070cb
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-19 13:00:04 +0100

    checker

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
+class BookList(request):
+    BookList = Book.objects.all()
+    serializer_class = BookSerializer
+    
