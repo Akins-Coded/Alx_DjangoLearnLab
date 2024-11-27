3d31c5fd6b71154ec3f6402fb2e91a7fd2598039
Akindipe Muheez <akindipemuheez@gmail.com>
2024-11-27 13:58:55 +0100

    checker

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Author(models.Model):
+class author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
