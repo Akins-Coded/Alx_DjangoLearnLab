c5251bbbc56fd89e2b9cf86114db1a0405bae38a
Akindipe Muheez <akindipemuheez@gmail.com>
2024-12-06 00:42:29 +0100

    checker

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('api/books/', BookListView.as_view(), name='book-list'),
     path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('api/books/create/', BookCreateView.as_view(), name='book-create'),
-    path('api/books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('api/books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('api/books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('api/books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
 
