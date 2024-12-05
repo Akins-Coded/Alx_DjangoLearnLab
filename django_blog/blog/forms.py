6ee1604218d5cc73615df3e5c595f6b5e3817668
Akindipe Muheez <akindipemuheez@gmail.com>
2024-12-05 21:44:56 +0100

    checker

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post, Comment, Tag
+from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -11,20 +12,16 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'tags')
+        fields = ('title', 'content', 'tags')  # Include the 'tags' field from the Post model
+        widgets = {
+            'tags': TagWidget(attrs={'class': 'form-control'}),  # Use the appropriate widget for tags
+        }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['tags'].widget = forms.CheckboxSelectMultiple(
-            choices=Tag.objects.all().values_list('id', 'name')
-        )
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('content', )
-
+        fields = ('content',)
