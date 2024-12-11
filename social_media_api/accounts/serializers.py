5c656ad3d6085c5ef79a01b04cd9cda4d62e4623
Akindipe Muheez <akindipemuheez@gmail.com>
2024-12-11 10:09:03 +0100

    Permissions

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,7 +10,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
         token, _ = Token.objects.create(user=user)
